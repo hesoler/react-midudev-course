8a043bb2a7f5ff7641c840eab982fe46aff341de
Héctor <hecalana@gmail.com>
2023-11-28 18:35:12 -0500

    Add third project

---
+++ projects/3_mouse-follower/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("effect", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) window.addEventListener("pointermove", handleMove);
+
+    // !cleanup
+    return () => window.removeEventListener("pointermove", handleMove);
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Deactivate" : "Activate"}
+      </button>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
+}
+
+export default App;
