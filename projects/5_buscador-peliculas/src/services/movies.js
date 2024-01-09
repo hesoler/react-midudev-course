a4b7fce34931d2f971079a1a43cbd20ee6bd8d63
Héctor <hecalana@gmail.com>
2024-01-09 01:08:00 -0500

    set https protocol for api url

--- projects/5_buscador-peliculas/src/services/movies.js
+++ projects/5_buscador-peliculas/src/services/movies.js
@@ -4,7 +4,7 @@ export const searchMovies = async ({ search }) => {
     if (search === '') return null
 
     try {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`)
         const json = await response.json()
 
         const movies = json.Search
