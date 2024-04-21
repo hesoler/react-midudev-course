255ae3fae4c9dbf23ba6ce8c35357d106594f4f5
Héctor Soler <hecalana@gmail.com>
2024-04-20 22:29:28 -0400

    Add  workspace for vitest extension

---
+++ vitest.workspace.js
@@ -0,0 +1,13 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  './projects/1_twitter-follow-card/vite.config.js',
+  './projects/2_tic-tac-toe/vite.config.js',
+  './projects/3_mouse-follower/vite.config.js',
+  './projects/4_prueba-tecnica/vite.config.js',
+  './projects/5_buscador-peliculas/vite.config.js',
+  './projects/6_shopping-cart/vite.config.js',
+  './projects/7_hesoler-router/vite.config.js',
+  './projects/8_todo-app-ts/vite.config.ts',
+  './projects/9_google-translate-clone/vite.config.ts'
+])
